b1f885c64fe7fcfc9fb96c0ed21f93fb2fd0c1b3
jamienj <48058224+jamienj@users.noreply.github.com>
2020-09-13 09:18:48 -0400

    corrected entry zone replace ip locations

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 sed -i "s/REPLACEIP/$FORWARDIP/g" /etc/bind/named.conf.options
-sed -i "s/REPLACEIP/$IP/g" /etc/bind/zones/nintendo.zone
-sed -i "s/REPLACEIP/$IP/g" /etc/bind/zones/nintendowifi.zone
+sed -i "s/REPLACEIP/$IP/g" /etc/bind/zones/nintendo.net
+sed -i "s/REPLACEIP/$IP/g" /etc/bind/zones/nintendowifi.net
 sed -i "s/REPLACEIP/$IP/g" /etc/bind/zones/90dns.test
 sed -i "s/REPLACEIP/$IP/g" /etc/bind/zones/generic
 named -c /etc/bind/named.conf
